ca578285d134575ce9eedfc43a83383d9eb8afa5
Pavel Kogan <pkogan@gmail.com>
2013-11-20 21:18:25 +0000

    Add handling for mutually exclusive options

--- Options.hs
+++ Options.hs
@@ -1,6 +1,16 @@
 module Options (Options(..), parseOpt) where
 
 import System.Console.GetOpt
+import Control.Applicative ((<$>), (<*>))
+import Control.Exception (assert)
+import Data.Maybe (isJust)
+
+class Switch a where
+  on, off :: a -> Bool
+  on  = not . off; off = not . on
+
+instance Switch Bool       where on = id
+instance Switch (Maybe a)  where on = isJust
 
 data Options = Options
   { optVerbose  :: Bool
@@ -38,6 +48,22 @@ options =
       "make symlinks relative"
   ]
 
+mutuallyExclusive :: [[Options -> Bool]]
+mutuallyExclusive = [ [optLink, optSymbolic]
+                    , [optVerbose, optQuiet] ]
+
+defaultsOK :: Bool
+defaultsOK = and $
+  (off .) <$> (concat mutuallyExclusive) <*> [defaultOptions]
+
 parseOpt :: [String] -> (Options, [String], [String])
-parseOpt argv = (foldl (.) id o defaultOptions, n, e)
-  where (o, n, e) = getOpt Permute options argv
+parseOpt argv = assert defaultsOK $ (o', n, e')
+  where
+    (o, n, e) = getOpt Permute options argv
+    o' = foldl (.) id o defaultOptions
+    e' = if not (collision o') then e
+           else "mutually exclusive options\n":e
+
+collision :: Options -> Bool
+collision o = or $ map g mutuallyExclusive
+  where g = (>1) . length . filter id . map (\f -> on $ f o)
